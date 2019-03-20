3b94c71a089fe1c2660b7c5590de2833c3f56fdd
ZhelinCheng <chengzhelin@foxmail.com>
2019-03-20 18:09:50 +0800

    图片信息生成

--- app.js
+++ app.js
@@ -9,13 +9,13 @@ const indexRouter = require('./routes/index');
 const app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'pug');
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+// app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
