e076f94ba28d53f29d541fb1230064233820e254
ZhelinCheng <chengzhelin@foxmail.com>
2020-03-12 12:44:25 +0800

    优化

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,20 +1,22 @@
 /*
  * @Author: Zhelin Cheng
  * @Date: 2019-09-23 10:15:33
- * @LastEditTime: 2019-11-26 22:21:36
+ * @LastEditTime: 2020-03-12 12:33:09
  * @LastEditors: Zhelin Cheng
  * @Description: PM2配置文件
  */
+const os = require('os')
 
 module.exports = {
   apps: [{
     name: process.env.SALAD_NAME || 'Salad',
     script: 'dist/main.js',
     // Options reference: https://doc.pm2.io/en/runtime/guide/ecosystem-file/#ecosystem-file
-    instances: 1,
+    instances: os.cpus().length,
+    exec_mode: 'cluster',
     autorestart: true,
     watch: false,
-    max_memory_restart: '1024M',
+    max_memory_restart: '350M',
     max_restarts: 3,
     env: {
       NODE_ENV: 'production'
